c1bd1637712ca20c2bf37e2fa9cb6c6271ef22a8
stud <stud@PK511-4>
2023-05-23 12:31:36 +0400

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public class Calculation
+        {
+            /// <summary>
+            /// рассчитывает свободные промежутки времени сотрудников
+            /// </summary>
+            /// <param name="startTimes"> начало </param>
+            /// <param name="durations"> продолжительность </param>
+            /// <param name="beginWorkingTime"> начало рабочего времени </param>
+            /// <param name="endWorkingTime"> конец рабочего времени </param>
+            /// <param name="consultationTime"> время на консультацию</param>
+            /// <returns> список свободных временных интервалов в графике сотрудника</returns>
+            public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime,
+                TimeSpan endWorkingTime, int consultationTime)
+            {
+                string[] h = Rabota(beginWorkingTime, endWorkingTime, consultationTime);
+
+
+                return h;
+            }
+
+            private static string[] Rabota(TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+            {
+               
+            }
+        }
+    }
+}
